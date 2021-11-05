41442a95aadae05e2010c9727341c3fc04a8492d
Rob Noyes <robertjnoyes1@gmail.com>
2021-11-05 14:07:51 -0700

    project card hover linking

--- src/components/home-components/ProjectCard.js
+++ src/components/home-components/ProjectCard.js
@@ -1,9 +1,35 @@
-const ProjectCard = ({ src, title, text, link }) => {
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import { MdArrowForwardIos } from 'react-icons/md';
+
+const ProjectCard = ({ src, title, color }) => {
+  const [hover, setHover] = useState(false);
+
   return (
-    <div className="project-card">
-      <img className="project-card-image" src={src}></img>
-      <h3>Project One</h3>
-      <p>Text For Project One </p>
+    <div
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className="project-card"
+    >
+      <CSSTransition
+        in={hover}
+        timeout={300}
+        classNames="project-caption"
+        unmountOnExit
+      >
+        <div className="project-caption" style={{ background: { color } }}>
+          <h3>Project</h3>
+          <Link to="/portfolio/test">
+            <div className="view-button">
+              <h3>View Project</h3>
+
+              <MdArrowForwardIos />
+            </div>
+          </Link>
+        </div>
+      </CSSTransition>
+      <img className="project-card-image" alt="" src={src}></img>
     </div>
   );
 };
