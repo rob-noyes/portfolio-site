e693148dc45eb5f7b950c5c16f1795d8437ca3e4
Rob Noyes <robertjnoyes1@gmail.com>
2021-11-10 10:27:50 -0800

    about page inital content

---
+++ src/components/about-components/Skills.js
@@ -0,0 +1,17 @@
+const Skills = () => {
+  return (
+    <div className="about-skills">
+      <h3>Skills</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>TailwindCSS</li>
+        <li>Javascript</li>
+        <li>ReactJS</li>
+        <li>Nodejs</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
