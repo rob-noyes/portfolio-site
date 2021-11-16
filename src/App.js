ac5a398caa6c98879646ed364307d6c6086f6e24
Rob Noyes <robertjnoyes1@gmail.com>
2021-11-16 10:28:58 -0800

    no need for react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 
 function App() {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
