28d63eedd59f8e48239f9622a93a016b9509d774
Rob Noyes <robertjnoyes1@gmail.com>
2022-01-07 11:04:17 -0800

    under construction

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const [menu, setMenu] = useState(true);
+  const [menu, setMenu] = useState(false);
 
   const menuControl = () => {
     setMenu(!menu);
@@ -11,24 +11,39 @@ const Header = () => {
   return (
     <div className=''>
       <div
-        className={
-          menu
-            ? ' bg-secondary fixed top-0 left-0 w-screen h-screen transition-all transform duration-300 '
-            : 'transition-all transform duration-300'
-        }
+        className={`
+          ${
+            menu ? 'absolute top-0 w-5/6 h-screen left-0' : 'w-0'
+          } bg-secondary transition-all transform duration-300`}
       >
-        <ul>
-          <li>Home</li>
+        <ul
+          className={
+            menu
+              ? 'pt-10 flex flex-col justify-center items-center text-3xl'
+              : 'hidden text-3xl'
+          }
+        >
+          <li className='py-4'>Home</li>
+          <li className='py-4'>Projects</li>
+          <li className='py-4'>About Me</li>
+          <li className='py-4'>Contact</li>
         </ul>
       </div>
-      <div className='flex justify-between items-center   bg-tertiary'>
-        <div className={menu ? '  fixed right-5 ' : 'flex'}>
-          <button onClick={menuControl}>Menu</button>
+      <div
+        className='
+          flex justify-between items-center bg-tertiary'
+      >
+        <div
+          className={`${
+            menu ? ' fixed right-20 ' : ' flex right-96'
+          } pl-5 transition-all transform duration-300 `}
+        >
+          <button onClick={menuControl}>{menu ? 'Close' : 'Menu'}</button>
         </div>
         <Link to='/'>
           <img
             src='/images/robNoyes.png'
-            className='w-44 cursor-pointer'
+            className='pr-5 w-44 cursor-pointer'
             alt=''
           />
         </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Home() {
-  return <div>This is the Homepage!</div>;
+  return (
+    <div className='flex justify-center items-center w-full bg-primary h-screen'>
+      Under Construction
+    </div>
+  );
 }
 
 export default Home;
