839e9ff7e28acd9f5683717d948a15185ea43975
Rob Noyes <robertjnoyes1@gmail.com>
2022-01-08 09:42:05 -0600

    navbar updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { MdMenu, MdClose } from 'react-icons/md';
 
 const Header = () => {
   const [menu, setMenu] = useState(false);
@@ -9,46 +10,50 @@ const Header = () => {
   };
 
   return (
-    <div className=''>
-      <div
-        className={`
-          ${
-            menu ? 'absolute top-0 w-5/6 h-screen left-0' : 'w-0'
-          } bg-secondary transition-all transform duration-300`}
-      >
-        <ul
-          className={
-            menu
-              ? 'pt-10 flex flex-col justify-center items-center text-3xl'
-              : 'hidden text-3xl'
-          }
-        >
-          <li className='py-4'>Home</li>
-          <li className='py-4'>Projects</li>
-          <li className='py-4'>About Me</li>
-          <li className='py-4'>Contact</li>
+    <nav className='bg-tertiary'>
+      <div className='flex justify-between items-center m-auto h-14 shadow-lg max-w-5xl'>
+        <button onClick={menuControl} className='pl-1 text-3xl sm:hidden'>
+          <MdMenu />
+        </button>
+        <ul className='hidden sm:flex text-white'>
+          <Link to='/'>
+            <li className='px-2'>Projects</li>
+          </Link>
+          <Link to='/'>
+            <li className='px-2'>About</li>
+          </Link>
+          <Link to='/'>
+            <li className='px-2'>Contact</li>
+          </Link>
         </ul>
-      </div>
-      <div
-        className='
-          flex justify-between items-center bg-tertiary'
-      >
-        <div
-          className={`${
-            menu ? ' fixed right-20 ' : ' flex right-96'
-          } pl-5 transition-all transform duration-300 `}
-        >
-          <button onClick={menuControl}>{menu ? 'Close' : 'Menu'}</button>
-        </div>
-        <Link to='/'>
+        <Link className='w-44' to='/'>
           <img
             src='/images/robNoyes.png'
-            className='pr-5 w-44 cursor-pointer'
+            className='pr-5 cursor-pointer'
             alt=''
           />
         </Link>
       </div>
-    </div>
+      <div
+        className={`${
+          menu
+            ? 'fixed mt-14 top-0 w-full h-32 z-10'
+            : 'w-full h-0 fixed top-14'
+        } bg-secondary transition-all transform duration-300 ease-in-out`}
+      >
+        <ul
+          className={
+            menu
+              ? 'flex flex-col items-center text-lg h-full'
+              : 'hidden text-lg'
+          }
+        >
+          <li className='py-2'>Projects</li>
+          <li className='py-2'>About Me</li>
+          <li className='py-2'>Contact</li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
