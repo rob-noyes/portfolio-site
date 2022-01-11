7e767739331a9028da907b4fa0d1924d738dd730
Rob Noyes <robertjnoyes1@gmail.com>
2022-01-10 22:16:00 -0800

    update to favicon and project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,14 +7,14 @@ function ProjectCard({ title, href, subTitle, src }) {
         <img
           src={src}
           alt={title}
-          className='opacity-90 hover:opacity-50 w-full object-cover'
+          className='opacity-90 hover:opacity-50 w-full'
         />
-        <div className='flex text-base font-medium py-2 '>
+        <div className='flex text-base md:text-xl font-medium  py-2 '>
           <h2>
-            {title} - <span className='font-normal'>{subTitle}</span>
+            {title} <span className='font-normal'>– {subTitle}</span>
           </h2>
         </div>
-        <span className='text-gray-700'>View Project</span>
+        <span className='text-gray-700 md:text-lg'>View Project</span>
       </Link>
     </div>
   );
